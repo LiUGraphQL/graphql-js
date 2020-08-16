4085da8b6fd35a8afe01b8ca834389d279973b6b
Robin Keskisärkkä <robin.keski@gmail.com>
2020-08-17 00:09:30 +0200

    update README and add script to publish to npm-dev branch

---
+++ gitpublish-dev.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+npm install @babel/core @babel/node @babel/preset-env @babel/cli --save-dev
+npm run build
+
+# Exit immediately if any subcommand terminated
+trap "exit 1" ERR
+
+# This script is used to maintain a git branch which mirrors the woosh-branch in a form that
+# allows npm dependencies to use:
+#
+#     "graphql": "git://github.com/LiUGraphQL/graphql-js.git#npm"
+#
+
+# Create empty npm directory
+rm -rf npm
+git clone -b npm-dev "git@github.com:LiUGraphQL/graphql-js.git" npm
+
+# Remove existing files first
+rm -rf npm/**/*
+rm -rf npm/*
+
+# Copy over necessary files
+cp -r dist/* npm/
+
+# Reference current commit
+HEADREV=`git rev-parse HEAD`
+echo $HEADREV
+
+# Deploy
+cd npm
+git add -A .
+if git diff --staged --quiet; then
+  echo "Nothing to publish"
+else
+  git commit -a -m "Deploy $HEADREV to npm-dev branch"
+  git push > /dev/null 2>&1
+  echo "Pushed"
+fi
